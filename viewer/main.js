435d5a0e7f5db1d4ac595c59bc05291b5b7c06de
jgaffuri <jgaf@proton.me>
2024-12-28 14:29:27 +0100

    a

--- viewer/main.js
+++ viewer/main.js
@@ -1,17 +1,27 @@
 import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+//import OSM from 'ol/source/OSM';
+import ImageTile from 'ol/source/ImageTile.js';
+
 
 const map = new Map({
   target: 'map',
   layers: [
-    new TileLayer({
+    /*new TileLayer({
       source: new OSM()
-    })
+    }),*/
+    new TileLayer({
+      source: new ImageTile({
+        url:
+          'https://raw.githubusercontent.com/jgaffuri/CartoHD_webmap/main/tiles/{z}/{x}/{y}.png',
+      }),
+    }),
   ],
   view: new View({
     center: [598290, 5357042],
-    zoom: 17
+    zoom: 19
   })
 });
+
+
